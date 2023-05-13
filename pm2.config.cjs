d58db6877808f9d9e3d0c46d9cc5df20ca87bf69
pathnirvana <pathnirvana@gmail.com>
2023-05-13 09:33:03 +0700

    adding pm2 config

---
+++ pm2.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+    apps : [
+        {
+            name: 'library',
+            script: 'node-server.js library-config.json',
+            cwd: '/home/janaka/file-server',
+            instances: 1,
+            cron_restart: '0 0 * * *', // once a day
+        },
+        {
+            name: 'cloud',
+            script: 'node-server.js cloud-config.json',
+            cwd: '/home/janaka/file-server',
+            instances: 1,
+        },
+    ]
+  }
